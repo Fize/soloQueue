9b5862c547fb2c3e2e3bd29174c18f2037fe4fe0
malzahar <malzaharguo@gmail.com>
2026-06-05 12:55:19 +0800

    refactor(todo): remove kanban issue management system and associated frontend components

--- web/src/components/PlansPage.tsx
+++
@@ -1,9 +0,0 @@
-import { Board } from './Board'
-
-export function PlansPage() {
-  return (
-    <div className="h-full px-3 py-3 sm:px-6 sm:py-6 flex flex-col min-h-0 overflow-hidden">
-      <Board />
-    </div>
-  )
-}
